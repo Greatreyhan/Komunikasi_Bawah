109a1c4be14ac269fefe0e4c8fb7bd3af4e6eb9d
Justrezy007 <joinreyhan@gmail.com>
2023-03-04 13:53:25 +0700

    first commit

---
+++ MDK-ARM/RTE/_RX_NewCom_407/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'RX_NewCom_407' 
+ * Target:  'RX_NewCom_407' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
